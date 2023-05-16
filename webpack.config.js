684904ad56925b49c93259419c3e4776dbafecfc
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-16 16:42:21 -0400

    linter fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),  
-    filename: 'output.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'output.bundle.js',
   },
   plugins: [
-      new HtmlWebPackPlugin()
-   ]
+    new HtmlWebPackPlugin(),
+  ],
 };
